ff7cfdfa49d825d795112f306cafa40bd7ddf815
LucasMartinez-Estudiante <lucas.martinez@est.fi.uncoma.edu.ar>
2025-09-01 18:14:02 -0300

    cambio estilos menu-header-footer

--- vista/TP/3/1/index.php
+++ vista/TP/3/1/index.php
@@ -8,33 +8,35 @@
 include_once '../../../estructura/header.php'
 ?>
 
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-
-            <div class="card shadow-sm">
-                <div class="card-body">
-                    <h3 class="card-title text-center mb-4">Subir archivo</h3>
-
-                    <form action="mostrar.php" method="post" enctype="multipart/form-data">
-                        <div class="mb-3">
-                            <label for="archivo" class="form-label">Seleccione un archivo (.pdf o .doc)</label>
-                            <input class="form-control" type="file" id="archivo" name="archivo" accept=".pdf,.doc">
-                        </div>
-
-                        <div class="d-grid">
-                            <button type="submit" class="btn btn-primary btn-block">
-                                <i class="bi bi-upload me-2"></i> Enviar
-                            </button>
-                        </div>
-                    </form>
-
+<main>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+    
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <h3 class="card-title text-center mb-4">Subir archivo</h3>
+    
+                        <form action="mostrar.php" method="post" enctype="multipart/form-data">
+                            <div class="mb-3">
+                                <label for="archivo" class="form-label">Seleccione un archivo (.pdf o .doc)</label>
+                                <input class="form-control" type="file" id="archivo" name="archivo" accept=".pdf,.doc">
+                            </div>
+    
+                            <div class="d-grid">
+                                <button type="submit" class="btn btn-primary btn-block">
+                                    <i class="bi bi-upload me-2"></i> Enviar
+                                </button>
+                            </div>
+                        </form>
+    
+                    </div>
                 </div>
+    
             </div>
-
         </div>
     </div>
-</div>
+</main>
 
 <?php
 include_once '../../../estructura/footer.php'
